55d0830a1c79dd3eba64ab09dba66569b95bfee9
temp939 <1852709704@qq.com>
2025-03-28 14:27:22 +0800

    Add files via upload

---
+++ JavaScript/IndexJavaScript.js
@@ -0,0 +1,270 @@
+﻿//每隔1000毫秒刷新当前获取的时间
+setInterval(function () {
+    //获取系统当前时间
+    var time = new Date();
+    //将时间格式化
+    var formattime = time.toLocaleString();
+    //将时间赋值给控件text
+    $("#a_date").text(formattime);
+}, 1000);
+
+$("#img_user").on("click", function () {
+    $("#myModal3").modal("show");
+});
+
+//判断是否登录
+setInterval(function () {
+    $.post("/Ajax/UserHandler.ashx?type=info", function (res) {
+        if (res.code == 1) {
+            $("body").show();
+            $("#text_Uname").text(res.data[0]);
+            $("#a_roles").text(res.data[1]);
+            $("#img_user").attr("src", "/Image/HeadPortrait/" + res.data[2]);
+            $("#img_usermodal").attr("src", "/Image/HeadPortrait/" + res.data[2])
+        } else {
+            location.href = "/html页面/登录页面/Login.html";
+            alert(res.msg);
+        }
+    });
+}, 1000); 
+
+//退出登录
+function exit() {
+    var bol = confirm("您确认要退出登录吗？");
+    if (bol) {
+        $.post("/Ajax/UserHandler.ashx?type=exit", function (res) {
+            if (res.code == 1) {
+                location.href = "/html页面/登录页面/Login.html";
+            }
+        });
+    }
+}
+
+    //从数据库中查询菜单
+    $.post("/Ajax/MenuHandler.ashx?type=GetMenuAut", function (res) {
+        if (res.code > 0) {
+            var ul = $('<ul class="list-group"></ul>').appendTo("#menu");
+            res.data.forEach(function (v) {
+                //父菜单-----------------------------------------------------------------------
+                if (v.ParentID == 0) {
+                    var li = $('<li class="menu-list"></li>').appendTo(ul);
+                    var dl = $("<dl></dl>").appendTo(li);
+                    var dt = $(`
+                            <dt>
+                                <img src="/Image/Menu/${v.Icon}" width="18" style="margin-right:5px;" />
+                                <label style="width:170px;cursor: pointer;">${v.Mname}</label>
+                                <span class="glyphicon glyphicon-menu-right"></span>
+                            </dt>`).on("click", function () {
+
+                                if ($(this).siblings().length > 0) {
+                                    //绑定点击父菜单事件
+                                    var $dd = $(this).siblings("dd");//选择this的同级dd元素
+                                    var $span = $(this).find(".glyphicon").attr("class");
+                                    if ($($dd).is(":hidden")) {
+                                        //显示
+                                        $(this).addClass("clickmenu");
+                                        $(this).find(".glyphicon").removeClass($span).addClass("glyphicon glyphicon-menu-down");
+                                    } else {
+                                        //隐藏
+                                        $(this).removeClass("clickmenu");
+                                        $(this).find(".glyphicon").removeClass($span).addClass("glyphicon glyphicon-menu-right");
+                                    }
+                                    //子菜单切换显示和隐藏
+                                    $dd.toggle(100);
+                                } else {
+                                    $(".menu-list dl dd").removeClass("Submenu");
+                                    $(".menu-list dl dd").find("a").css("color", "white");
+                                    
+                                    $(".menu-list dl dt").css("color", "#BDC2C8");
+                                    $(".menu-list dl dt").removeClass("Submenu");
+                                    $('dt').each(function () {
+                                        if ($(this).find("img").attr("src").indexOf("默认(黑)") !== -1) {
+                                            $(this).find("img").attr("src", "/Image/Menu/默认.png");
+                                        }
+                                    }); 
+
+                                    $(this).addClass("Submenu");
+                                    $(this).css("color", "black");
+                                    $(this).find("img").attr("src","/Image/Menu/默认(黑).png");
+                                    $("#li_home").removeClass("active");
+                                    $("#myiframes").attr("src", v.Murl);
+
+                                }
+
+                            }).bind({
+                        //绑定鼠标悬浮在父菜单事件
+                        mouseover: function () {
+                            $(this).find(".glyphicon").css("color", "chartreuse");
+                            $(this).addClass("hovermenu");
+                        },
+                        mouseout: function () {
+                            $(this).find(".glyphicon").css("color", "#BDC2C8");
+                            $(this).removeClass("hovermenu");
+                        }
+                    }).appendTo(dl);//将dt附加到dl中
+
+                    //子菜单-----------------------------------------------------------------------
+                    res.data.forEach(function (vv) {
+                        if (vv.ParentID == v.Mid) {
+                            $("<dd><a>" + vv.Mname + "</a></dd>").on("click", function () {
+                                //绑定子菜单点击事件
+                                $(".menu-list dl dd").removeClass("Submenu");
+                                $(".menu-list dl dd").find("a").css("color", "white");
+
+                                $("dt").removeClass("Submenu");
+                                $("dt").css("color", "#BDC2C8");
+                                $('dt').each(function () {
+                                    if ($(this).siblings().length > 0) {
+                                    } else {
+                                        $(this).find("span").removeClass("glyphicon glyphicon-menu-right");
+                                    }
+                                }); 
+
+                                $(this).addClass("Submenu");
+                                $(this).find("a").css("color", "black");
+                                $("#li_home").removeClass("active");
+                                $("#myiframes").attr("src", vv.Murl);
+
+                                $('dt').each(function () {
+                                    if ($(this).find("img").attr("src").indexOf("默认(黑)") !== -1) {
+                                        $(this).find("img").attr("src", "/Image/Menu/默认.png");
+                                    }
+                                }); 
+                            }).bind({
+                                //绑定鼠标悬浮在子菜单事件
+                                mouseover: function () {
+                                    $(this).addClass("hovermenu");
+                                },
+                                mouseout: function () {
+                                    $(this).removeClass("hovermenu");
+                                }
+                            }).appendTo(dl);//将dd附加到dl中
+                        }//vv.ParentID
+                    });//res.data vv
+                }//v.ParentID
+            });//res.data v
+            $(`<div style="width:100%;height:100px;"></div>`).appendTo("#menu");
+
+            $('dt').each(function () {
+                if ($(this).siblings().length > 0) {
+                } else {
+                    $(this).find("span").removeClass("glyphicon glyphicon-menu-right");
+                }
+            }); 
+        }//res.code
+    });//post
+
+
+//点击首页事件
+
+//ready等待网页上的元素全部准备完毕
+$(document).ready(function () {
+    //点击首页事件
+    $("#li_home").on("click", function () {
+        $(this).addClass("active");
+        $(".menu-list dl dd").removeClass("Submenu");
+        $(".menu-list dl dd").find("a").css("color", "white");
+        $(".menu-list dl dt").removeClass("Submenu");
+        $(".menu-list dl dt").css("color", "#BDC2C8");
+        $('dt').each(function () {
+            if ($(this).find("img").attr("src").indexOf("默认(黑)") !== -1) {
+                $(this).find("img").attr("src", "/Image/Menu/默认.png");
+            }
+        }); 
+    });
+});
+
+//侧边显示隐藏切换
+$("#btn_left").on("click", function () {
+    var windowWidth = $(window).width();
+    $(".left").toggle();
+    if ($('.left').is(':visible')) {
+        $("#btn_left").attr("src","/Image/Index/开.png");
+        if (windowWidth < 768) {
+            $('.left').css({ "position": "absolute", "z-index": "10" });
+            $('.right').css("width", "100%");
+            $("#div_show").show();
+        } else {
+            $('.right').css("width", "calc(100% - 250px)");
+        }
+    } else {
+        $("#btn_left").attr("src", "/Image/Index/关.png");
+        $('.right').css("width", "100%");
+    }
+
+});
+
+$(document).ready(function () {
+    var windowWidth = $(window).width();
+
+    if (windowWidth < 768) {
+        $("#btn_left").attr("src", "/Image/Index/关.png");
+        $(".left").hide();
+        $('.right').css("width", "100%");
+    } else {
+        $("#btn_left").attr("src", "/Image/Index/开.png");
+        $(".left").show();
+        $('.right').css("width", "calc(100% - 250px)");
+    }
+    $(window).resize(function () {
+        windowWidth = $(window).width();
+        if (windowWidth < 768) {
+            $("#btn_left").attr("src", "/Image/Index/关.png");
+            $(".left").hide();
+            $('.right').css("width", "100%");
+        } else {
+            $("#btn_left").attr("src", "/Image/Index/开.png");
+            $(".left").show();
+           $('.left').css({ "position": "initial", "z-index": "0" });
+            $('.right').css("width", "calc(100% - 250px)");
+            $("#div_show").hide();
+
+        }
+    });
+});
+
+$("#div_show").on("click", function () {
+    $("#btn_left").attr("src", "/Image/Index/关.png");
+    $("#div_show").hide();
+    $('.right').css("width", "100%");
+    $(".left").hide();
+});
+
+$("#index_updpwd").on("click", function () {
+    $("#myModalPwd").modal("show");
+});
+
+//设置密码的显示与隐藏
+function PwdOrShowHide(that) {
+    var labelClass = $(that).find("label").attr("class");
+    $(that).find("label").removeClass(labelClass).
+        addClass(labelClass == "glyphicon glyphicon-eye-close" ? "glyphicon glyphicon-eye-open" : "glyphicon glyphicon-eye-close");
+    $(that).prev().attr("type", labelClass == "glyphicon glyphicon-eye-open" ? "password" : "text");
+}
+
+$("#btn_verification").on("click", function () {
+    var pwd = $("#text_pwd").val();
+    var newpwd = $("#text_newpwd").val();
+    var newpwd2 = $("#text_newpwd2").val();
+    if (pwd != "" && newpwd != "" && newpwd2 != "") {
+        if (newpwd == newpwd2) {
+            $.post("/Ajax/UserHandler.ashx?type=SelPwdUid", { pwd: pwd }, function (res) {
+                if (res.code != 0) {
+                    $.post("/Ajax/UserHandler.ashx?type=IndexUpdPwd", { pwd: newpwd2 }, function (res) {
+                        if (res.code > 0) {
+                            $("#myModalPwd").modal("hide");
+                        }
+                        alert(res.msg);
+                    });
+                } else {
+                    alert("原密码不正确！");
+                }
+            });
+        } else {
+            alert("两次密码不一致！");
+        }
+    } else {
+        alert("输入框的信息不能为空！");
+    }
+});
+
---
+++ JavaScript/LoginJavaScript.js
@@ -0,0 +1,181 @@
+﻿//轮播图
+//ready等待网页上的元素全部准备完毕
+$(document).ready(function () {
+    if ($.cookie("Phone") && $.cookie("Pwd")) {
+        $("#ipt_phone").val($.cookie("Phone"));
+        $("#ipt_pwd").val($.cookie("Pwd"));
+        $(".remember").prop("checked", true);
+    }
+
+    var index = 0;
+    function img() {
+        var $img = document.querySelector(".img"); //获取.img元素
+        var w = getComputedStyle($img).width; //获取.imgcenter的宽
+        w = Number(w.slice(0, -2)); //计算
+        $img.querySelector(".img-imgcenter").style.left = index * w * -1 + "px";
+    }
+    //获取.img-imgcenter下有多少子元素
+    var imgnum = $(".img-imgcenter").children().length;
+    //每隔4秒切换一次图片
+    setInterval(function () {
+        index++;
+        if (index > imgnum - 1) {
+            index = 0;
+        }
+        img();
+    }, 4000);
+
+});
+
+//密码显示隐藏
+$("#pwd_ico").on("click", function () {
+    var $class = $(this).attr("class");
+    $(this).removeClass($class).
+        addClass($class == "glyphicon glyphicon-eye-close" ? "glyphicon glyphicon-eye-open" : "glyphicon glyphicon-eye-close")
+    $(this).prev().attr("type", $class == "glyphicon glyphicon-eye-open" ? "password" : "text");
+});
+
+
+//点击登录按钮事件
+$("#btn_login").on("click", function () {
+    var phone = $("#ipt_phone").val();
+    var pwd = $("#ipt_pwd").val();
+    if (phone != "" && pwd != "") {
+        $.post("/Ajax/UserHandler.ashx?type=login", {
+            phone: phone,
+            pwd: pwd
+        }, function (res) {
+            if (res.code == 1) {
+                $.post("/Ajax/MenuHandler.ashx?type=GetMenuAut", function (res) {
+                    if (res.code > 0) {
+                        Remember(phone, pwd);
+                        location.href = "/html页面/主框架/Index.html";
+                    } else {
+                        Warn(res.msg);
+                    }
+                })
+            } else {
+                Warn(res.msg);
+            }
+        });
+    } else {
+        Warn("输入框信息不能为空！");
+    }
+});
+
+
+//光标聚焦输入框事件
+$('#ipt_phone,#ipt_pwd').focus(function () {
+    $("#div_warn").hide();
+    $("#warn_text").text("");
+});
+
+
+//提示框
+function Warn(text) {
+    $("#div_warn").show();
+    $("#warn_text").text(text);
+}
+
+
+//判断是否选中记住我
+function Remember(phone, pwd) {
+    if ($(".remember").is(":checked")) {
+        $.cookie("Phone", phone, { expires: 1, path: "/" });
+        $.cookie("Pwd", pwd, { expires: 1, path: "/" });
+    } else {
+        $.removeCookie("Phone", { path: "/" });
+        $.removeCookie("Pwd", { path: "/" });
+    }
+}
+
+//点击忘记密码
+$("#a_pwd").on("click", function () {
+    $("#myModal2").modal("show");
+})
+
+//设置密码的显示与隐藏
+function PwdOrShowHide(that) {
+    var labelClass = $(that).find("label").attr("class");
+    $(that).find("label").removeClass(labelClass).
+        addClass(labelClass == "glyphicon glyphicon-eye-close" ? "glyphicon glyphicon-eye-open" : "glyphicon glyphicon-eye-close");
+    $(that).prev().attr("type", labelClass == "glyphicon glyphicon-eye-open" ? "password" : "text");
+}
+
+var numcode = 0;
+var regex = /^1[3456789]\d{9}$/;
+//更换电话号码 确认
+$("#btn_verification2").on("click", function () {
+    var phone = $("input[name=userphone]").val();
+    var code = $("input[name=code]").val();
+    if (phone != "" && code != "") {
+        if (regex.test(phone)) {
+            if (code == numcode) {
+                Phones=phone;
+                $("input[name=userphone]").val("");
+                $("input[name=code]").val("");
+                $("#myModal2").modal("hide");
+                $("#myModal3").modal("show");
+            } else {
+                alert("验证码错误，请重新输入！");
+            }
+        } else {
+            alert("请输入正确的手机号码！");
+        }
+    } else {
+        alert("输入框的信息不能为空！");
+    }
+});
+
+var Phones = "";
+//获取验证码
+$("#btn_code").on("click", function () {
+    if ($(this).text() == "获取验证码") {
+        if ($("input[name=userphone]").val() != "" && regex.test($("input[name=userphone]").val())) {
+
+            $.post("/Ajax/UserHandler.ashx?type=SelPhone", { phone: $("input[name=userphone]").val()  }, function (res) {
+                if (res.code != 0) {
+                    numcode = Math.floor(Math.random() * 1000000);
+                    while (numcode.toString().length < 6) {
+                        numcode *= 10;
+                    }
+                    alert(numcode);
+                    var second = 30;
+                    var time = setInterval(function () {
+                        if (second == 0) {
+                            $("#btn_code").text("获取验证码");
+                            clearTimeout(time);
+                        } else {
+                            $("#btn_code").text(second + "s");
+                            second -= 1;
+                        }
+                    }, 1000);
+                } else {
+                    alert("当前输入的手机号码不存在账户，无法验证没有存在账户的手机号！");
+                }
+            });
+        } else {
+            alert("请输入正确的手机号码！");
+        }
+    }
+});
+
+//设置密码
+$("#btn_verification").on("click", function () {
+    var pwd = $("#text_newpwd").val();
+    var pwd2 = $("#text_newpwd2").val();
+    if (pwd != "" && pwd2 != "") {
+        if (pwd == pwd2) {
+            $.post("/Ajax/UserHandler.ashx?type=UpdPwd", { pwd: pwd2, phone: Phones }, function (res) {
+                if (res.code>0) {
+                    $("#myModal3").modal("hide");
+                }
+                alert(res.msg);
+            });
+        } else {
+            alert("两次密码输入不一致！");
+        }
+    } else {
+        alert("输入框的信息不能为空！");
+    }
+});
